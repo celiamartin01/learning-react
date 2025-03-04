0afab1d1222c8494dff48ac1de3c464ad4485dcd
celiamartin01 <celiamartinfuentes01@gmail.com>
2025-03-04 19:26:35 +0100

    learning what's context, aadded user context

--- projects/106-router/src/pages/HomePage.jsx
+++ projects/106-router/src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import HeaderComponent from '../components/HeaderComponent'
+import { useContext } from 'react'
+import { UserContext } from '../contexts/user.context'
 
 function HomePage() {
+  const {user, setUser} = useContext(UserContext)
   return (
     <>
         <HeaderComponent/>
         <h3>HomePage</h3>
+        <p>Welcome, {user}</p>
     </>
     
   )
