bdfe6e37da65dac8790936a84991332a924d0b65
celiamartin01 <celiamartinfuentes01@gmail.com>
2025-03-06 18:48:03 +0100

    Learning what is lazy loading

--- projects/106-router/src/App.jsx
+++ projects/106-router/src/App.jsx
@@ -1,16 +1,25 @@
 import './App.css'
+import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import BlogPage from './pages/BlogPage'
-import HomePage from './pages/HomePage'
-import PostDetails from './pages/PostDetails'
-import SettingsPage from './pages/SettingsPage'
 
-import ErrorPage from './pages/ErrorPage'
+import HomePage from './pages/HomePage'
+
+/* This is a way to render pages as they are needed, instead of render all of them
+just as we join the app. THIS IS NOT RECOMMENDED in general. We, as users, are used
+to "single page applications", where we wait a few seconds in the beginning to have
+a fluid navigation between pages. This way, using lazy, we will have loading time
+each time we navigate through pages */
+const BlogPage = lazy(() => import('./pages/BlogPage'))
+const PostDetails = lazy(() => import('./pages/PostDetails'))
+const SettingsPage = lazy(() => import('./pages/SettingsPage'))
+const ErrorPage = lazy(() => import('./pages/ErrorPage'))
 
 function App() {
 
   return (
-    <Routes>
+    // Fallback attribute is used to show an html element while the page is loading
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Routes>
       { /* Quite self explanatory: we set the path where the element is being rendered
         So, "/" is the root element, the page that will show when you open the app
         Then in "/blog" the blog page will appear. Now we are rendering this app at
@@ -22,6 +31,8 @@ function App() {
 
       <Route path="/*" element={<ErrorPage />} />
     </Routes>
+    </Suspense>
+    
   )
 }
 
