59fde9c7177a5d1314f3e162f7ff7d0f78839c17
celiamartin01 <celiamartinfuentes01@gmail.com>
2025-03-06 18:18:28 +0100

    Using react memo for posts in blogPage

--- projects/106-router/src/components/PostCard.jsx
+++ projects/106-router/src/components/PostCard.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function PostCard(props) {
+  console.log("Post rendered")
+
     const {post} = props
   return (
     <article>
@@ -13,4 +15,7 @@ function PostCard(props) {
   )
 }
 
-export default PostCard
+/* This is memoization: we keep those posts storaged in memory once they're rendered,
+so they're not rendered again when a state changes in that component if it does not 
+affect these posts */
+export default React.memo(PostCard)
--- projects/106-router/src/pages/BlogPage.jsx
+++ projects/106-router/src/pages/BlogPage.jsx
@@ -3,9 +3,11 @@ import HeaderComponent from '../components/HeaderComponent'
 import { useEffect, useState, useContext } from 'react'
 import PostCard from '../components/PostCard'
 import { BlogContext } from '../contexts/blog.context'
+import { UserContext } from '../contexts/user.context'
 
 function BlogPage() {
   const {posts, error, getPosts} = useContext(BlogContext)
+  const {login, logout} = useContext(UserContext)
 
   useEffect(() => {
     getPosts()
@@ -22,6 +24,15 @@ function BlogPage() {
   return (
     <>
       <HeaderComponent />
+
+      { /* We display this as an example of memoization: when the user state changes in this
+      component, the whole component is rendered again, including all the posts, even when
+      that posts information has not changed. To avoid that, we can use react memo to keep
+      posts stored in memory so our app doesn't have to render it over and over when it's not
+      changed ..?? i guess this is the correct explanation */}
+      <button onClick = {login}>Login</button>
+      <button onClick = {logout}>Log out</button>
+
       <h3>Blog page</h3>
 
       {/* Conditional render to show error message or loading message in case our request 
