200600cac956e78cef23a4dc14e7ca3accfd96f2
celiamartin01 <celiamartinfuentes01@gmail.com>
2025-03-07 18:00:24 +0100

    Configured test enviroment, jest and react testing library, created a HomePage test file with a dumb test just to check the enviroment, everything works

---
+++ projects/106-router/src/pages/HomePage.test.js
@@ -0,0 +1,8 @@
+// import HomePage from "./HomePage"
+
+describe("HomePage", () => {
+    it("Simple test", () => {
+        // This is a stupid test just to check if the test enviroment works
+        expect(1).toEqual(1)
+    })
+})
